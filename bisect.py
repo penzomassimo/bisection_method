eed5e4a91e2ff031230eff2d0604e5b64ddb4c25
Massimo Penzo <penzo.massimo@gmail.com>
2019-01-13 16:36:58 -0600

    improved the

--- bisect.py
+++ bisect.py
@@ -1,27 +1,25 @@
-import sys
+# Purpose:	Find the roots of a polynomial function via the bisection numerical method
+# Author:	Massimo Penzo
 
+# defining the function we are trying to find the roots for
 def f(x):
-	return x**3 + x -1
-	
-def bisection(a,b,tol):
-	c = (a+b)/2.0
-	while (b-a)/2.0 > tol:
-		if f(c) == 0:
-			return c
+	return x**2 - 2
+
+# bisect algorithm implementation
+def bisect(a, b, tolerance):
+
+	c = (a + b) / 2
+	while (b - a) / 2 > tolerance:
+
+		if f(a)*f(c) > 0:
+			a = c
 		elif f(a)*f(c) < 0:
 			b = c
 		else :
-			a = c
-		c = (a+b)/2.0
+			return c
+		c = (a + b) / 2
 		
-	return c
-	
-def main(argv):
-	if (len(sys.argv) != 4):
-		sys.exit('Usage: bisection.py <a> <b> <tol>')
-	
-	print 'The root is: ',
-	print bisection(int(sys.argv[1]),int(sys.argv[2]),float(sys.argv[3]))
+	return c	
 
-if __name__ == "__main__":
-	main(sys.argv[1:])
+root = bisect(-100, 100, 0.0003)
+print(root)
